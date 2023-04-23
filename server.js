311c533f6fe9c612bb0c752c15cc2a6c836584e2
Mirac Murat Karakas <109850327+miracmuratkarakas@users.noreply.github.com>
2023-04-23 18:46:03 +0300

    v1.0 Güncellemesi
    Sözdizimine uygun fixlendi.

--- server.js
+++ server.js
@@ -10,11 +10,9 @@ const io = socket (server)
 
 io.on('connection', (socket) => {
     console.log(socket.id)
-
     socket.on('chat', data => {
         io.sockets.emit('chat', data)
     })
-
     socket.on('typing', data => {
         socket.broadcast.emit('typing', data)
     })
